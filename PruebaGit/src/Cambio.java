ec00905696f431c108287b392a39773077804fae
Alberto <albertolopezpatricio@gmail.com>
2020-11-26 13:28:32 +0100

    Segundo commit

--- PruebaGit/src/Cambio.java
+++ PruebaGit/src/Cambio.java
@@ -3,7 +3,7 @@ public class Cambio {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("HOLA MUNDO");
+			System.out.println("HOLA MUNDO SOY ALBERTO");
 	}
 
 }
